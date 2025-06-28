9d43736a53cd9c70981a2b7fd4364aff941f6e59
muchaomuicho <muchaomuicho@gmail.com>
2025-06-29 06:27:43 +0800

    Site updated: 2025-06-29 06:27:42

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/06/28/hello-world/","2025/06/28/test/"];function toRandomPost(){
+var posts=["2025/06/28/test/","2025/06/28/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
