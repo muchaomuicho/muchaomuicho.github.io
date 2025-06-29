78d7dcdc13b4ebe696118ca4ad8b30f0c20356aa
muchaomuicho <muchaomuicho@gmail.com>
2025-06-29 10:18:32 +0800

    Site updated: 2025-06-29 10:18:32

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/06/28/hello-world/","2025/06/29/对于原神的一些个人见解/"];function toRandomPost(){
+var posts=["2025/06/29/对于原神的一些个人见解/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
