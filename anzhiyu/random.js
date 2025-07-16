d6e633201067c1efc40681c0bf222b6699496141
muchaomuicho <muchaomuicho@gmail.com>
2025-07-16 20:23:35 +0800

    Site updated: 2025-07-16 20:23:35

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["p/subapi/","p/2025-7-1/","p/对于原神的一些个人见解/","p/809/"];function toRandomPost(){
+var posts=["p/2025-7-1/","p/809/","p/对于原神的一些个人见解/","p/subapi/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
